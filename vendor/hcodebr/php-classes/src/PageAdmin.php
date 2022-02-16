a8bcd9d2c1c29dd01f042c8e9affb79c8a628991
gumaath <guuhsykees@gmail.com>
2022-02-16 11:30:49 -0300

    create page admin and tpl

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Hcode;
+
+class PageAdmin extends Page {
+    
+    public function __construct($opt = array(), $tpl_dir = "/views/admin/") {
+        parent::__construct($opt, $tpl_dir);
+    }
+}
+
+?>
