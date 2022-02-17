41534b1dac2e9c6fb59a228befa440cf5afe7e65
gumaath <guuhsykees@gmail.com>
2022-02-17 13:30:27 -0300

    create crud done

--- index.php
+++ index.php
@@ -63,6 +63,13 @@ $app->get("/admin/users/create", function() {
 
 $app->get("/admin/users/:iduser/delete", function($iduser) {
     User::verifyLogin();
+    $user = new User();
+
+    $user->get((int)$iduser);
+    $user->delete();
+    
+    header("Location: /admin/users");
+    exit;
 });
 
 $app->get("/admin/users/:iduser", function($iduser) {
@@ -85,16 +92,16 @@ $app->post("/admin/users/create", function() {
     exit;
 });
 
-//$app->post("/admin/users/:iduser", function($iduser) {
-//  User::verifyLogin();
-//   $user = new User();
-//   $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
-//   $user->get((int)$iduser);
-//  $user->setData($_POST);
-//  $user->update();
-//  header("Location: /admin/users");
-//  exit;
-//});
+$app->post("/admin/users/:iduser", function($iduser) {
+  User::verifyLogin();
+  $user = new User();
+  $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+  $user->get((int)$iduser);
+  $user->setData($_POST);
+  $user->update();
+  header("Location: /admin/users");
+  exit;
+});
 
 $app->run();
 
